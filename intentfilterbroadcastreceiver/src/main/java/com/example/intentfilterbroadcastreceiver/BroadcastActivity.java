fe1a5a7ce4584d8508d13c1c1097ee015fbd4578
aniruddha <aniruddha.kudalkar@gmail.com>
2018-12-12 19:46:43 +0530

    dialog : time picker hw : home work for fragment PDF Attached statusbar notifications : covered many aspects of status bar notification broadcast receiver : creating and using broadcast receiver

---
+++ intentfilterbroadcastreceiver/src/main/java/com/example/intentfilterbroadcastreceiver/BroadcastActivity.java
@@ -0,0 +1,52 @@
+package com.example.intentfilterbroadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+public class BroadcastActivity extends AppCompatActivity {
+
+    private IntentFilter filterFlight;
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i("@codekul", "Intent called");
+            Boolean sts = intent.getBooleanExtra("state", false);
+            if(sts) {
+                ((ImageView)findViewById(R.id.img)).setImageResource(R.drawable.ic_airplanemode_active_black_24dp);
+            }else {
+                ((ImageView)findViewById(R.id.img)).setImageResource(R.drawable.ic_airplanemode_inactive_black_24dp);
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_broadcast);
+
+        filterFlight = new IntentFilter();
+        filterFlight.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+
+        sendBroadcast(new Intent());
+    }
+
+    /*@Override
+    protected void onStart() {
+        super.onStart();
+
+        registerReceiver(receiver, filterFlight);
+    }
+
+    @Override
+    protected void onStop() {
+
+        unregisterReceiver(receiver);
+        super.onStop();
+    }*/
+}
--- intentfilterbroadcastreceiver/src/main/java/com/example/intentfilterbroadcastreceiver/MainActivity.kt
+++ intentfilterbroadcastreceiver/src/main/java/com/example/intentfilterbroadcastreceiver/MainActivity.kt
@@ -7,15 +7,14 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    // Everything from intent should find the match
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btOk.setOnClickListener {
-            implicit()
+           call()
         }
-
     }
 
     fun explicit() {
@@ -38,6 +37,14 @@ class MainActivity : AppCompatActivity() {
         val int = Intent(Intent.ACTION_MAIN)
         int.addCategory(Intent.CATEGORY_LAUNCHER)
         int.data= Uri.parse("file:///data/data/xyx.mp3")
+        startActivity(int)
+    }
+    fun call() {
+//        val int = Intent(Intent.ACTION_DIAL)
+        //val int = Intent(Intent.ACTION_CALL)
+
+        val int = Intent(Intent.ACTION_VIEW)
+        int.data = Uri.parse("http://codekul.com")
         startActivity(int)
     }
 }
